adb1248db2cdef0e2fdee0018efd5265cc06a21a
vishvanithin <vishvanithin862@gmail.com>
2023-01-13 12:46:55 +0530

    new change

---
+++ Array.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Array {
+	
+	public static void main(String[] args) {
+	
+	    int a=20;
+	    int b=30;
+	    int sum;
+	    
+	    sum=a-~b-1;
+			
+	    System.out.println(~b);
+		
+	}
+	
+
+}
